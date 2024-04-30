6e7f13cfd7e4a081c02c716cd1ee0000852f626d
httgiang <httgiang.gdsciu@gmail.com>
2024-04-30 16:01:46 +0700

    Giang | Add UI for login page and logic behind 2 pages

--- src/main/java/com/example/ui/SignUpController.java
+++ src/main/java/com/example/ui/SignUpController.java
@@ -1,7 +1,12 @@
 package com.example.ui;
+import javafx.animation.TranslateTransition;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Duration;
 
 import javax.swing.*;
 import java.sql.Connection;
@@ -15,43 +20,140 @@ public class SignUpController {
     private TextField address;
 
     @FXML
-    private TextField mobile;
+    private Button forgotPassword;
 
     @FXML
-    private TextField password;
+    private Button li_logInButton;
+
+    @FXML
+    private TextField li_password;
+
+    @FXML
+    private Button li_signUpButton;
+
+    @FXML
+    private TextField li_userEmail;
+
+    @FXML
+    private TextField mobile;
 
     @FXML
     private TextField pincode;
 
     @FXML
-    private TextField userEmail;
+    private Button su_logInButton;
+
+    @FXML
+    private TextField su_password;
+
+    @FXML
+    private Button su_signUpButton;
+
+    @FXML
+    private TextField su_userEmail;
 
     @FXML
     private TextField userName;
+    @FXML
+    private AnchorPane sideForm;
+
+    @FXML
+    private AnchorPane signUpPane;
+    @FXML
+    private AnchorPane logInPane;
+
+    @FXML
+    private void switchForm(ActionEvent event) {
+        TranslateTransition slider = new TranslateTransition();
+
+        if (event.getSource() == li_signUpButton) {
+            slider.setNode(sideForm);
+            slider.setToX(-600);
+            slider.setDuration(Duration.seconds(.7));
+            slider.setAutoReverse(true);
+
+//            slider.setOnFinished((ActionEvent e) ->{
+//                logInPane.setVisible(false);
+//            });
+            slider.play();
+
+        } else if (event.getSource() == su_logInButton){
+            slider.setNode(sideForm);
+            slider.setToX(0);
+            slider.setDuration(Duration.seconds(.7));
+            slider.play();
+        }
+    }
 
     Connection con = null;
     ResultSet rs = null;
     PreparedStatement pst = null;
+    private Alert alert;
 
     @FXML
-    public void signUp(ActionEvent event) throws Exception{
-        con = MySQLConnection.connectDb();
-        String query = "INSERT INTO USERS(name, email, mobile, address, pincode, password) VALUES(?,?,?,?,?,?)";
-        try{
-            pst = con.prepareStatement(query);
-            pst.setString(1, userName.getText());
-            pst.setString(2, userEmail.getText());
-            pst.setInt(3, Integer.parseInt(mobile.getText()));
-            pst.setString(4, address.getText());
-            pst.setInt(5, Integer.parseInt(pincode.getText()));
-            pst.setString(6, password.getText());
-            pst.execute();
+    public void logIn(){
+        if(li_userEmail.getText().isEmpty() || li_password.getText().isEmpty()){
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please enter your email and password!");
+            alert.showAndWait();
+        } else {
+            con = MySQLConnection.connectDb();
+            String select = "SELECT email, password FROM USERS WHERE email = ? AND password = ?";
+            try{
+                pst = con.prepareStatement(select);
+                pst.setString(1, li_userEmail.getText());
+                pst.setString(2, li_password.getText());
+                rs = pst.executeQuery();
+                if(rs.next()){
+                    alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("Information Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Successfully login!");
+                    alert.showAndWait();
+                } else {
+                    alert = new Alert(Alert.AlertType.ERROR);
+                    alert.setTitle("Error Message");
+                    alert.setHeaderText(null);
+                    alert.setContentText("Incorrect email or password");
+                    alert.showAndWait();
+                }
+            } catch (Exception e){
+                e.printStackTrace();
+            }
 
-            JOptionPane.showMessageDialog(null, "Information saved");
-        } catch (Exception e) {
-            JOptionPane.showMessageDialog(null, e);
+
         }
-
     }
+    @FXML
+    public void signUp(ActionEvent event) throws Exception {
+        if(userName.getText().isEmpty() || su_userEmail.getText().isEmpty() || mobile.getText().isEmpty() ||
+            address.getText().isEmpty() || pincode.getText().isEmpty() || su_password.getText().isEmpty()){
+            alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error Message");
+            alert.setHeaderText(null);
+            alert.setContentText("Please fill in all the blank fields!");
+            alert.showAndWait();
+        } else {
+            con = MySQLConnection.connectDb();
+            String insert = "INSERT INTO USERS(name, email, mobile, address, pincode, password) VALUES(?,?,?,?,?,?)";
+            try {
+                pst = con.prepareStatement(insert);
+                pst.setString(1, userName.getText());
+                pst.setString(2, su_userEmail.getText());
+                pst.setInt(3, Integer.parseInt(mobile.getText()));
+                pst.setString(4, address.getText());
+                pst.setInt(5, Integer.parseInt(pincode.getText()));
+                pst.setString(6, su_password.getText());
+                pst.execute();
 
+                JOptionPane.showMessageDialog(null, "Information saved");
+            } catch (Exception e) {
+                JOptionPane.showMessageDialog(null, e);
+            }
+        }
+    }
 }
+
+
